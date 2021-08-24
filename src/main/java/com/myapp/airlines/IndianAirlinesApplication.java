3fc5d1b45d32c4cddd3654b845c81b69968d9c41
DebjotiMallick <debjoti97@gmail.com>
2021-08-24 18:27:39 +0530

    Project Created

---
+++ src/main/java/com/myapp/airlines/IndianAirlinesApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.airlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndianAirlinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndianAirlinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapp/airlines/IndianAirlinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.airlines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndianAirlinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
